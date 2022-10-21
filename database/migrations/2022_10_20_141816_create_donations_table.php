135f46b6f67a299c4c161841d591f6984ac6cd35
R-Almayyas <108932239+RashedAlmayyas@users.noreply.github.com>
2022-10-21 21:12:31 +0200

    Add files via upload

---
+++ database/migrations/2022_10_20_141816_create_donations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->integer('card_num')->unique();
+            $table->timestamp('date');
+            $table->float('amount');
+            $table->timestamps();
+            $table->rememberToken();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+};
