7040516490f50cf05c9eee8b4e9ed76c3b90b8b3
AhmadyZahran <ahmadyousefzahran@gmail.com>
2022-10-24 22:33:40 +0300

    vv

--- resources/views/volunteers/register.blade.php
+++ resources/views/volunteers/register.blade.php
@@ -6,9 +6,9 @@
       data-wow-delay="0.1s"
     ></div>
 <section class="vh-10  my-5 bg-light" >
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-lg-12 col-xl-11">
+    <div class="container h-100 ">
+      <div class="row d-flex justify-content-center align-items-center h-100 ">
+        <div class="col-lg-12 col-xl-11 ">
           <div class="card text-black" style="border-radius: 25px;">
             <div class="card-body p-0">
               <div class="row justify-content-center">
@@ -25,7 +25,7 @@
                     @csrf
                     @method('POST')
                     <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                      <i class="fas fa-user fa-lg me-3 fa-fw mt-4 pt-2"></i>
                       <div class="form-outline flex-fill mb-0">
                           <label class="form-label" for="form3Example1c">الاسم </label>
                         <input type="text" id="form3Example1c" class="form-control" name="name" value="{{old('name')}}" />
@@ -38,7 +38,7 @@
                     </div>
   
                     <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                      <i class="fas fa-envelope fa-lg me-3 fa-fw mt-4 pt-2"></i>
                       <div class="form-outline flex-fill mb-0">
                           <label class="form-label" for="form3Example3c">بريد الالكتروني</label>
                         <input type="email" id="form3Example3c" class="form-control" name="email" value="{{old('email')}}" />
@@ -51,7 +51,7 @@
                     </div>
 
                     <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                        <i class="bi bi-telephone-fill fa-lg me-3 fa-fw mt-4 pt-2"></i>
                         <div class="form-outline flex-fill mb-0">
                             <label class="form-label" for="form3Example1c">رقم الهاتف</label>
                           <input type="number" name="phone" id="form3Example1c" class="form-control"  />
@@ -64,7 +64,7 @@
                       </div>
   
                     <div class="d-flex flex-row align-items-center mb-4">
-                      <i style="margin-bottom: " class="  fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <i style="margin-bottom: " class="  bi bi-image fa-lg me-3 fa-fw mt-4 pt-2"></i>
                       <div class="form-outline flex-fill mb-0">
                           <label class="form-label" for="form3Example4cc">الصورة الشخصية </label>
                         <input type="file" id="form3Example4cc" class="form-control" name="profile_image"/>
@@ -77,7 +77,7 @@
                     </div>
 
                     <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                        <i class="fas fa-lock fa-lg me-3 fa-fw mt-4 pt-2"></i>
                         <div class="form-outline flex-fill mb-0">
                             <label class="form-label" for="form3Example4c">كلمة المرور</label>
                           <input type="password" id="form3Example4c" class="form-control" name="password" />
@@ -90,7 +90,7 @@
                       </div>
   
                     <div class="d-flex flex-row align-items-center mb-3">
-                      <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                      <i class="fas fa-key fa-lg me-3 fa-fw mt-4 pt-2"></i>
                       <div class="form-outline flex-fill mb-0">
                           <label class="form-label" for="form3Example4cd">تأكيد كلمة المرور</label>
                         <input type="password" id="form3Example4cd" class="form-control" name="password_confirmation" />
@@ -120,8 +120,8 @@
                 </div>
                 <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
   
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                    class="img-fluid" alt="Sample image">
+                  <img src="https://cdn.discordapp.com/attachments/1032593113778245635/1034122657865666560/pexels-julia-larson-6456137.jpg"
+                    class="img-fluid w-100" alt="Sample image">
   
                 </div>
               </div>
